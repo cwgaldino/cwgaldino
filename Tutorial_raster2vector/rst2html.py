47e687916efe8d2918a9c19f0811aee3b28bf2c9
cwgaldino <carlosw.galdino@gmail.com>
2020-06-29 10:14:20 -0300

    p

--- Tutorial_raster2vector/rst2html.py
+++ Tutorial_raster2vector/rst2html.py
@@ -6,23 +6,20 @@ import docutils.core
 from pathlib import Path
 import sys
 
-def rst2html(source_path, destination_path=None):
+def rst2html(source_path, destination_path=None, css=None):
     if destination_path is None:
         destination_path = str(Path(source_path).parent/(Path(source_path).name.split('.')[0] + '.html'))
 
-    settings_overrides = {'stylesheet_path' : 'test.css'}
-
-    docutils.core.publish_file(source_path      = source_path,
-                               destination_path = destination_path,
-                               writer_name ="html",
-                               settings_overrides = settings_overrides)
-
-# help(docutils.core.publish_file)
-# rst2html(source_path='test.rst', destination_path=None)
+    if css is not None:
+        settings_overrides = {'stylesheet_path' : css}
+        docutils.core.publish_file(source_path      = source_path,
+                                   destination_path = destination_path,
+                                   writer_name ="html",
+                                   settings_overrides = settings_overrides)
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 3:
-        rst2html(source_path=sys.argv[1], destination_path=sys.argv[2])
+        rst2html(source_path=sys.argv[1], destination_path=None, css=sys.argv[2])
     else:
         rst2html(source_path=sys.argv[1])
